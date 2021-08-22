f68c84c62115359c537bde04585c44b39baf8ce5
Chris Greene <archgoon@gmail.com>
2021-08-22 12:51:36 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='ashnazg',
+   version='1.0',
+   description='And in the darkness bind them',
+   author_email='archgoon+ashnazg@gmail.com',
+   packages=['ashnazg', 'ashnazg.analyses'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
