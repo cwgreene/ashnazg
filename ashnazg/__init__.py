47752aef7e40d5ee2263b6bef34e9717a33f6fbc
Chris Greene <archgoon@gmail.com>
2022-01-12 16:05:52 -0800

    Add in dynamic versioning.

--- ashnazg/__init__.py
+++ ashnazg/__init__.py
@@ -1,1 +1,2 @@
 from .ashnazg import *
+from .version import __version__
--- setup.py
+++ setup.py
@@ -1,12 +1,36 @@
 from setuptools import setup
+import codecs
+import os.path
+import subprocess
+
 
 # because the python ecosystem is stupid.
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+def set_version(rel_path):
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), rel_path)
+    result = subprocess.check_output(["git","rev-parse","HEAD"]).strip().decode("ascii")
+    with open(path, "w") as version_file:
+        version_file.write(f"__version__='{result}'")
+    return result
+
 setup(
    name='ashnazg',
-   version='1.0',
+   version=set_version("ashnazg/version.py"),
    description='And in the darkness bind them',
    author_email='archgoon+ashnazg@gmail.com',
    packages=['ashnazg', 'ashnazg.analyses'],  #same as name
